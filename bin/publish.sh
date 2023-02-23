d509945fe1f3825ce3b9f9a477b2593abed04e3b
an.orsich <an.orsich@gmail.com>
2023-02-23 13:24:32 +0000

    [-] Adjust build platform

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker build -t anorsich/shopify-krane .
+docker build --platform linux/amd64 -t anorsich/shopify-krane .
 
 docker image tag anorsich/shopify-krane anorsich/shopify-krane:3.0.1
 docker image push anorsich/shopify-krane:3.0.1
